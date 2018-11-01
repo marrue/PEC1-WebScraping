bb40d9d6088fe9c3cc84b2b6f5bb175a01cdd5f5
marrue <44439631+marrue@users.noreply.github.com>
2018-11-01 13:53:22 +0100

    Add files via upload

--- webScraping.py
+++ webScraping.py
@@ -11,27 +11,19 @@ soup = BeautifulSoup(page.content)
 # Busqueda del elemento que contiene la información
 data = soup.find(class_= "contenidoTiempo")
 
-# Extracción de la toda información del primer día
-day1 = data.find('a', attrs={"href":"/Actividad.nsf/frmWeb?ReadForm&idioma=cas&dia=1"})
-
-# Selección de la información de interes del primer día
-
-day_name = day1.find(class_="fctDayDate").get_text()
-day_temp = day1.find(class_="fctHiLow").get_text()
-day_cond = day1.find(class_="fctDayConditions").get_text()
-day_rain = day1.find(class_="popValue").get_text()
-day_text = day1.find(class_="popText").get_text()
-
-# Mostramos por pantalla el resultado de la extracción
-print(day_name)
-print(day_temp)
-print(day_cond)
-print(day_rain)
-print(day_text)
+# Definición de la función de extracción de información
+def extract_info(day_href):
+    day_info = data.find ('a', attrs={"href":day_href})
+    day_name = day_info.find(class_="fctDayDate").get_text()
+    day_temp = day_info.find(class_="fctHiLow").get_text()
+    day_cond = day_info.find(class_="fctDayConditions").get_text()
+    day_rain = day_info.find(class_="popValue").get_text()
+    day_text = day_info.find(class_="popText").get_text()
+    return(day_name,day_temp,day_cond,day_rain,day_text)
 
 # Definición de la función de extracción de imagenes
 def load_requests(source_url):
-    r = requests.get(source_url, stream = True )
+    r = requests.get(source_url, stream = True)
     if r.status_code == 200:
         aSplit = source_url.split('/')
         ruta = "C:/Users/Javier/1.-Master/CURSO18-19/1er_cuatri/Tipología y ciclo de vida de los datos/PRAC1/imagenes/"+aSplit[len(aSplit)- 1]
@@ -40,6 +32,17 @@ def load_requests(source_url):
         for chunk in r:
             output.write(chunk)
         output.close()
+
+# Extradcción de la información del primer dia
+href = "/Actividad.nsf/frmWeb?ReadForm&idioma=cas&dia=1"
+[day_name,day_temp,day_cond,day_rain,day_text] = extract_info(href)
+
+# Mostramos por pantalla la información extraída
+print(day_name)
+print(day_temp)
+print(day_cond)
+print(day_rain)
+print(day_text)
 
 # Extracción de la imagen correspondiente al primer día
 load_requests("https://www.donostia.eus/taxo/imagenes/chancerain.gif")
